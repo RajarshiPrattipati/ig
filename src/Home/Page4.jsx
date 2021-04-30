7add23bfa25804f5476f33a171cb8686a592e34a
rishi <rishiprattipati@gmail.com>
2021-04-30 19:49:11 +0530

    2

--- src/Home/Page4.jsx
+++ src/Home/Page4.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {  Row,  Col} from 'antd';
 import { page4 } from './data';
-import img from "./Web 6.jpg" ;
+
 
 
 export default function Page4() {
   const children = page4.children.map((img, i) => (
     <Col md={6} xs={24} key={i.toString()} className="page4-item">
-      <img src={img} alt="img" />
+
     </Col>
   ));
   return (
